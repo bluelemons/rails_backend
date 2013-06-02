f6eae4ab182b70ac92f47baf431debb97ddc6e84
olvap <oldani.pablo@gmail.com>
2013-06-01 21:00:21 -0300

    Add backend application

--- app/controllers/rails_backend/application_controller.rb
+++ app/controllers/rails_backend/application_controller.rb
@@ -1,4 +1,10 @@
 module RailsBackend
-  class ApplicationController < ActionController::Base
+  class ApplicationController < InheritedResources::Base
+
+    protected
+      def collection
+        @q = end_of_association_chain.search(params[:q])
+        get_collection_ivar || set_collection_ivar(@q.result(distinct: true).paginate(:page => params[:page]))
+      end
   end
 end
