d950b7e6c5c0645d334aeffb47d43b7d8bdd7e0e
Pablo Oldani <oldani.pablo@gmail.com>
2013-06-06 17:36:12 -0300

    add breadcrumbs funtionality in controllers

--- app/controllers/backend/application_controller.rb
+++ app/controllers/backend/application_controller.rb
@@ -3,6 +3,8 @@ module Backend
 
     helper_method :attributes
 
+    before_filter :add_breadcrumbs
+
     def attributes
       resource_class.attribute_names - %w(id created_at updated_at)
     end
@@ -11,6 +13,18 @@ module Backend
       def collection
         @q = end_of_association_chain.search(params[:q])
         get_collection_ivar || set_collection_ivar(@q.result(distinct: true).paginate(:page => params[:page]))
+      end
+
+      def paths
+        request.path.split("/")[1..-1]
+      end
+
+      def add_breadcrumbs
+        url = ""
+        paths.each do |path|
+          url = url + "/" + path
+          add_breadcrumb path, url
+        end
       end
   end
 end
