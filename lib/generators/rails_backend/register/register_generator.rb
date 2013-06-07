0e52a966754929dd8ef0f7d1f43bf7648534414b
Pablo Oldani <oldani.pablo@gmail.com>
2013-06-07 12:14:33 -0300

    Remove symbolick links and replace for the path.

--- lib/generators/rails_backend/register/register_generator.rb
+++ lib/generators/rails_backend/register/register_generator.rb
@@ -1,5 +1,5 @@
 class RailsBackend::RegisterGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../../../../../app', __FILE__)
+  source_root File.expand_path('../templates', __FILE__)
 
   def generate
     template "controller.rb", "app/controllers/backend/#{plural_name}_controller.rb"
