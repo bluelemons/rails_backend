03fb053071271ab4cc6eadf1d018c2e57912ccf9
Eloy Espinaco <eloyesp@gmail.com>
2014-02-21 16:08:56 -0300

    Remove deprecated config settings.
    - Remove whiny_nils option.
    - Add new eager load setting.
    - Remove whitelist attributes.
    - Update secret token.

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '30296030fbc777f0f1c899a3a6fb12a97f43ec163d811f99cd95a1696b1a5c6ae29409d249bda20483dbda415a005e40ff561f991a7d1bff9bebc79b1cf8e78c'
+Dummy::Application.config.secret_key_base = '30296030fbc777f0f1c899a3a6fb12a97f43ec163d811f99cd95a1696b1a5c6ae29409d249bda20483dbda415a005e40ff561f991a7d1bff9bebc79b1cf8e78c'
