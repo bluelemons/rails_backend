f7ce9c046f3f42c6f3ed0bdd84dbc803dcabb840
Pablo Oldani <oldani.pablo@gmail.com>
2014-08-06 23:23:34 -0300

    Fix test env.

--- test/dummy/test/unit/helpers/bootstrap_helper_test.rb
+++ test/dummy/test/unit/helpers/bootstrap_helper_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 
 class BootstrapHelperTest < ActionView::TestCase
 
-  test 'should create a breadcrumb' do
-    class Request
-      def path
-        "backend/blogs/1"
-      end
+  class Request
+    def path
+      "backend/blogs/1"
     end
-    
-    request = Request.new
-    assert_equal breadcrumb(request), "<li><a href='/backend'>Backend</a></li><li><a href='/backend/blogs'>Blogs</a></li><li class='active'>1</li>"
+  end
+
+  test 'should create a breadcrumb' do
+    path = Request.new
+    response = breadcrumb(path)
+    assert_equal response, "<li><a href=\"/backend\">Backend</a></li><li><a href=\"/backend/blogs\">Blogs</a></li><li class=\"active\">1</li>"
   end
 
 end
