9deea100698ea7892dfee84e7de5e44783479104
Pablo Oldani <oldani.pablo@gmail.com>
2014-08-06 22:45:11 -0300

    Fix breadcrumb.

--- lib/rails_backend/helpers/bootstrap_helper.rb
+++ lib/rails_backend/helpers/bootstrap_helper.rb
@@ -1,15 +1,47 @@
 module BootstrapHelper
 
-  def breadcrumb request
-    paths = request.path.split("/")
-    response = ""
-    paths.each do |path|
-      if paths.last == path
-        response += "<li class='active'>#{path}</li>"
-      else
-        response += "<li><a href='/#{ path }'>#{ path }</a></li>"
+  class Breadcrumb
+
+    include ActionView::Helpers::TagHelper
+
+    def initialize request
+      @request = request
+      @response = ""
+    end
+
+    def print
+      build
+      @response
+    end
+
+    private
+
+    def build
+      paths.each_with_index do |path, i|
+        @response += if paths.last == path
+          build_last path
+        else
+          build_row path, i
+        end
       end
     end
+
+    def build_last path
+      content_tag(:li, path, class: 'active')
+    end
+
+    def build_row path, i
+      content_tag(:li, content_tag(:a, path.capitalize, href: "/#{ paths[0..i].join('/') }"))
+    end
+
+    def paths
+      @request.path.split("/")
+    end
+
+  end
+
+  def breadcrumb request
+    raw Breadcrumb.new(request).print
   end
 
 end
